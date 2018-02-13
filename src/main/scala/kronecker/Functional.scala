60ca26ded4fb113ba38caad40716b4d17b42f803
Erik Osheim <d_m@plastic-idolatry.com>
2018-02-12 23:46:02 -0500

    Fix Countable[Map[K, V]], move towards Countable[A => B].
    The previous strategy for enumerating all map values was
    fundamentally busted. Our new strategy is easier to show
    correct and doesn't seem to have gaps or duplicates.
    
    In the same vein as Countable[Map[K, V]], I've started
    laying the ground-work for Countable function instances.
    Using kronecker.Functional we could write a method
    generating Countable[Z => B] given a Finite[B].
    
    To create Countable[A => B] for other types we need
    some kind of Indexable[A] instance that allows us to
    go from an A value to its index. Then we could implement
    (Indexable[A], Finite[B]) => Countable[A => B].
    
    For both maps and functions, the best solution for dealing
    with an infinite value type continues to elude me.

---
+++ src/main/scala/kronecker/Functional.scala
@@ -0,0 +1,73 @@
+package kronecker
+
+import scala.annotation.tailrec
+
+/**
+ * Used to generate arbitrary function values.
+ *
+ * For example, consider:
+ *
+ *   sealed trait Quad
+ *   case object A extends Quad
+ *   case object B extends Quad
+ *   case object C extends Quad
+ *   case object D extends Quad
+ *
+ *   Quad => Boolean
+ *
+ * There are 4 Quad values, and 2 Boolean values, so there are 16
+ * (2^4) functions of type Quad => Boolean. Each function can be
+ * uniquely represented as a value 0-15 (i.e. in 4 bits):
+ *
+ *    0 (0000) -- returns false for all inputs
+ *    1 (0001) -- returns true for A, false for all other inputs
+ *    2 (0010) -- returns true for B, false for all other inputs
+ *    3 (0011) -- returns true for A and B, false for all other inputs
+ *    4 (0100) -- returns true for C, false for all other inputs
+ *       ....
+ *   15 (1111) -- returns true for all inputs
+ *
+ * (Not all types have a cardinality that is a power of two, but using
+ * examples that do simplifies the above notation.)
+ *
+ * The idea here is that `index` specifies which function we want.
+ * When actually want to evaluate the function for a given `input`, we
+ * step through the index until we find the section that refers to the
+ * given input value (0 would be first). Once there, we find the value
+ * modulo `outWidth` and use that to determine which output value to
+ * use.
+ *
+ * The `evaluate` function is general purpose and uses the division
+ * and remainder operations. In `fastEvaluate` we special-case powers
+ * of two to use bit-shifting and masking, which are significantly
+ * faster for large numbers.
+ */
+object Functional {
+
+  def function1(index: Z, outWidth: Z): Function1[Z, Z] =
+    if (outWidth.toBigInt.bitCount == 1) {
+      // outWidth is a power of two
+      val mask = outWidth - 1
+      val shift = outWidth.bitLength - 1
+      (input: Z) => fastEvaluate(index, input, mask, shift)
+    } else {
+      // outWidth is not a power of two
+      (input: Z) => evaluate(index, input, outWidth)
+    }
+
+  def fastEvaluate(index: Z, input: Z, outMask: Z, outShift: Int): Z = {
+    @tailrec def loop(index0: Z, argIndex: Z): Z =
+      if (index0.isZero) Z.zero
+      else if (argIndex >= input) index0 & outMask
+      else loop(index0 >> outShift, argIndex + 1)
+    loop(index, Z.zero)
+  }
+
+  def evaluate(index: Z, input: Z, outWidth: Z): Z = {
+    @tailrec def loop(index0: Z, argIndex: Z): Z =
+      if (index0.isZero) Z.zero
+      else if (argIndex >= input) index0 % outWidth
+      else loop(index0 / outWidth, argIndex + 1)
+    loop(index, Z.zero)
+  }
+}
